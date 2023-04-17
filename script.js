0d52df26e6483430cfbfa986c92953dca6880345
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2023-04-17 09:12:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ const pan = document.getElementById('pan')
 let translate = document.getElementById('translate')
 
 putz.addEventListener('click', function() {
+    let audio = document.getElementById('audio')
+    audio.play()
+    let click1 = document.getElementById('click1')
+    click1.play()
     let sentence = document.getElementById('sentence')
     sentence.innerText = 'putzing around.'
     putz.style.backgroundColor = 'lime'
     putz.style.color = 'green'
+    putz.disabled = 'true'
+    put.disabled = 'true'
+    pan.disabled = 'true'
 })
 
 put.addEventListener('click', function() {
+    let click2 = document.getElementById('click2')
+    click2.play()
+    let oops1 = document.getElementById('oops1')
+    oops1.play()
     let img1 = document.getElementById('img1')
     img1.style.opacity = '0'
     let img2 = document.getElementById('img2')
@@ -19,13 +30,17 @@ put.addEventListener('click', function() {
     setTimeout(() => {
         img1.style.opacity = '1'
         img2.style.opacity = '0'
-    }, 1000);
+    }, 500);
     put.style.backgroundColor = 'red'
     put.style.color = 'yellow'
-    
+    put.disabled = 'true'
 })
 
 pan.addEventListener('click', function() {
+    let click3 = document.getElementById('click3')
+    click3.play()
+    let oops2 = document.getElementById('oops2')
+    oops2.play()
     let img1 = document.getElementById('img1')
     img1.style.opacity = '0'
     let img2 = document.getElementById('img2')
@@ -34,12 +49,15 @@ pan.addEventListener('click', function() {
     setTimeout(() => {
         img1.style.opacity = '1'
         img2.style.opacity = '0'
-    }, 1000);
+    }, 500);
     pan.style.backgroundColor = 'red'
     pan.style.color = 'yellow'
+    pan.disabled = 'true'
 })
 
 translate.addEventListener('click', function() {
+    let click2 = document.getElementById('click2')
+    click2.play()
     let sentence = document.getElementById('sentence')
     translate.innerText = 'Ela tá afim de não fazer nada.'
     setTimeout(() => {
